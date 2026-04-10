3890f52dbc705de7d269ee3f31e41990400f7c9f
anujedits42-byte <anujedits42@gmail.com>
2026-04-10 17:08:55 +0530

    Update start.py
    Signed-off-by: anujedits42-byte <anujedits42@gmail.com>

--- anony/modules/start.py
+++ anony/modules/start.py
@@ -1,12 +1,10 @@
-from pyrogram import filters, types
-
-from anony import app, buttons, db
-
-
 @app.on_message(filters.command(["start"]) & filters.private)
 async def f_start(_, m: types.Message):
-    await m.reply_text(
-        text=f"Hey {m.from_user.first_name},\n\nThis is {app.mention},\nAn open source session generator bot.",
+    await app.send_photo(
+        chat_id=m.chat.id,
+        photo=Translation.PHOTO_URL,
+        caption=Translation.START_TXT.format(m.from_user.first_name),
         reply_markup=buttons.start_key(),
+        parse_mode=enums.ParseMode.HTML
     )
     await db.add_user(m.from_user.id)
