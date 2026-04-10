b97431c009a090a95df6b08d98fa28f9431698e2
anujedits42-byte <anujedits42@gmail.com>
2026-04-10 16:04:26 +0530

    Update config.py
    Signed-off-by: anujedits42-byte <anujedits42@gmail.com>

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ load_dotenv()
 
 BOT_TOKEN = getenv("BOT_TOKEN", "8676646717:AAGfnpz2tjYoTqrJMi5Dv0yX33_ZJGqbKfc")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
+
 OWNER_ID = int(getenv("OWNER_ID",7892805795))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/log_channel_a")
