babc6e4c8ffab921c837d3fb1b95275a522cfd22
anujedits42-byte <anujedits42@gmail.com>
2026-04-10 18:46:23 +0530

    Update start.py
    Signed-off-by: anujedits42-byte <anujedits42@gmail.com>

--- anony/modules/start.py
+++ anony/modules/start.py
@@ -1,16 +1,10 @@
 @app.on_message(filters.command(["start"]) & filters.private)
 async def f_start(_, m: types.Message):
-    await app.send_photo(
-        chat_id=m.chat.id,
-        photo="https://files.catbox.moe/kpfuks.jpg",
-        caption=f"""<b>👋 Hello {m.from_user.first_name}</b>
-
-🤖 Welcome to Session Generator Bot
-
-🔥 Generate Pyrogram & Telethon Session easily!
-
-👇 Click below to start""",
-        reply_markup=buttons.start_key(),
-        parse_mode=enums.ParseMode.HTML
-    )
-    await db.add_user(m.from_user.id)
+await app.send_photo(
+chat_id=m.chat.id,
+photo=Translation.PHOTO_URL,
+caption=Translation.START_TXT.format(m.from_user.first_name),
+reply_markup=buttons.start_key(),
+parse_mode=enums.ParseMode.HTML
+)
+await db.add_user(m.from_user.id)
