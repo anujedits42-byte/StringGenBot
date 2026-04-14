ceca40866fe34c38b2f28b2cd77dcd6b944931f5
anujedits42-byte <anujedits42@gmail.com>
2026-04-14 23:09:26 +0530

    Update start.py
    Signed-off-by: anujedits42-byte <anujedits42@gmail.com>

--- anony/modules/start.py
+++ anony/modules/start.py
@@ -1,13 +1,13 @@
 from pyrogram import filters, types
-
 from anony import app, buttons, db
 
 PHOTO_URL = "https://o.uguu.se/buNPcLrC.jpg"
 
 @app.on_message(filters.command(["start"]) & filters.private)
 async def f_start(_, m: types.Message):
-    await m.reply_text(
-        text=f"Hey {m.from_user.first_name},\n\nThis is {app.mention},\nAn open source session generator bot.",
+    await m.reply_photo(
+        photo=PHOTO_URL,
+        caption=f"Hey {m.from_user.first_name},\n\nThis is {app.mention},\nAn open source session generator bot.",
         reply_markup=buttons.start_key(),
     )
     await db.add_user(m.from_user.id)
